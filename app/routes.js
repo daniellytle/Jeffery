ccf1024a7b87a65c0f8024a5499822d248965d55
Dan Wilson <wilsoda@umich.edu>
2016-04-21 13:32:01 -0400

    i

--- app/routes.js
+++ app/routes.js
@@ -2,6 +2,7 @@ module.exports = function(app, messenger, content) {
 
     // New Groupme Message
 	app.post('/group-message', function(req, res) {
+		console.log(req.body)
 		if (req.body.text == "@jeffery inspire") {
 			content.getQuote(function(quote) {
 				messenger.postMessage(quote, function(msg) {

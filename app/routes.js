8bb7e3a6ddd62dfb29642cc4e435daed9143a60d
Dan Wilson <wilsoda@umich.edu>
2016-04-21 23:15:01 -0400

    changed jeffy name

--- app/routes.js
+++ app/routes.js
@@ -1,20 +1,19 @@
 module.exports = function(app, messenger, content) {
 
-	var prefix = "@jeffery";
+	var prefix = "jeffy";
     // New Groupme Message
 	app.post('/group-message', function(req, res) {
-		console.log(req.body)
+		console.log(req.body);
 		var text = req.body.text;
-		if (req.body.text.substring(0,8) == prefix) {	
-			if (req.body.text == "@jeffery inspire") {
+		if (text.indexOf(prefix) > -1) {	
+			if (req.body.text == "jeffy inspire") {
 				content.getQuote(function(quote) {
 					messenger.postMessage(quote, function(msg) {
 						console.log(msg);
 						res.status(200).end();
 					})
 				})
-			} else {
-				text = text.substring(9);
+			} else {				
 				console.log(text);
 				content.getResponse(text, function(err, response, data) {
 					console.log(data)
